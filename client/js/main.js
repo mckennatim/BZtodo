442e40bf847e026f55b53a9626b5f989032c2033
Timothy McKenna <mckenna.tim@gmail.com>
2014-04-02 20:54:28 -0400

    add ctrl and view, use localStorage

--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,62 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
+
+  ctrl.loadList();
 
   $('form').submit(function(){
     var newItem = $('input').val();
+    if(newItem.length>1){
+      ctrl.addItem(newItem);
+    }
+    $('input').val('');
+    return false; 
+  });
+  
+
+});
+
+var ctrl ={
+  list: {BZtodo:[]},
+  priority: ['high', 'normal', 'low'],
+  defaultPriority: 'normal',
+  addItem: function(item){
+    this.add2list(item);
+  },
+  add2list:function(item){
+    var itemObject ={
+      item: item,
+      done: false,
+      priority: this.priority.indexOf(this.defaultPriority)
+    }
+    view.appendLi(itemObject);
+    this.list.BZtodo.push(itemObject);
+    this.saveList();
+  },
+  saveList: function(){
+    var listJ=JSON.stringify(this.list);
+    localStorage.setItem('BZtodo', listJ);     
+  },
+  loadList: function(){
+    this.list= JSON.parse(localStorage.BZtodo);
+    view.refreshList(this.list.BZtodo);
+  }
+
+} 
+
+var view ={
+  appendLi: function(itemObj){
+    var item = itemObj.item;
+    var priority = ctrl.priority[itemObj.priority];
     $('ul').append('<li>'
       +'<input type="checkbox"/>'
-      +'<span class="item"> '+ newItem +'</span>'
-      +'<span class="priority"> nornal </span>'
+      +'<span class="item"> '+ item +'</span>'
+      +'<span class="priority">' + priority +'</span>'
       +'<a href="">x</a>'
       +'</li>');
-    return false; 
-  })
-});
+  },
+  refreshList: function(list){
+    $.each(list, function(index, itemObj) {
+      console.log(itemObj);
+      view.appendLi(itemObj);
+    });
+  }
+}
